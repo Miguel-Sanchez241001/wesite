82a2e1fa783f822da8525fb3596ab509959f6199
Miguel-Sanchez241001 <msanchez24@autonoma.edu.pe>
2023-12-22 12:19:44 -0500

    slider y userMiguel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,34 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import {SidebarModule} from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import {MatListModule} from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,UserComponent],
+  imports: [MatListModule,MatIconModule,
+       CommonModule, RouterOutlet,UserComponent,SidebarModule,ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'angular17-web-site';
+  display = false;
+  typesOfShoes: string[] = ['Miguel', 'about', 'skill','workes',];
+
+  
+   
+ 
+    closeSidebar(){
+      this.display = false;
+    }
+
+    openSidebar(){
+      this.display = true;
+    }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { GamesComponent } from '../games/games.component';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [CommonModule,GamesComponent],
+  imports: [CommonModule,GamesComponent,CardModule,ButtonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
 
-  islogueado = true;
-  name = "juan";
+  
+    name = "Miguel Ivan Sanchez Sanchez";
+    job = "Dev Backend with Java";
+
+
+
 }
